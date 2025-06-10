08b1a2dee314fdd0e5a4d416b903e031aa74dc95
Accicom <centralaccicom@gmail.com>
2025-06-10 10:20:30 -0300

    cambios de ancho en "clientes"

--- src/pages/admin/CatalogClients.tsx
+++ src/pages/admin/CatalogClients.tsx
@@ -279,7 +279,7 @@ export default function CatalogClients() {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="max-w-[88rem] mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex items-center justify-between mb-6">
           <div className="flex items-center">
             <button
